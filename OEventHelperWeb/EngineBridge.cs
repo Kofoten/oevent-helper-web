e865551b247077cd78c93c00723adf4bb93ad50f
Rasmus Steen <rasmus@steen.ninja>
2026-04-01 23:35:52 +0200

    setup project

---
+++ OEventHelperWeb/EngineBridge.cs
@@ -0,0 +1,41 @@
+﻿using OEventCourseHelper.Core.CoursePrioritizer;
+using System;
+using System.IO;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OEventHelperWeb;
+
+public record UnexpectedError(string ErrorMessage);
+
+[JsonSerializable(typeof(CoursePrioritizerResult))]
+[JsonSerializable(typeof(UnexpectedError))]
+public partial class EngineJsonContext : JsonSerializerContext
+{
+}
+
+public partial class EngineBridge
+{
+    [JSExport]
+    public static string Prioritize(byte[] iofXmlBytes, int beamWidth, bool strict, string[] filter)
+    {
+        try
+        {
+            var engine = new CoursePrioritizerEngine(beamWidth, strict, filter);
+
+            CoursePrioritizerResult result;
+            using (var stream = new MemoryStream(iofXmlBytes))
+            {
+                result = engine.Run(stream);
+            }
+
+            return JsonSerializer.Serialize(result, EngineJsonContext.Default.CoursePrioritizerResult);
+        }
+        catch (Exception ex)
+        {
+            var error = new UnexpectedError(ex.Message);
+            return JsonSerializer.Serialize(error, EngineJsonContext.Default.UnexpectedError);
+        }
+    }
+}
---
+++ OEventHelperWeb/wwwroot/main.js
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './_framework/dotnet.js'
+
+const { getAssemblyExports, getConfig } = await dotnet.create();
+const config = getConfig();
+
+const exports = await getAssemblyExports(config.mainAssemblyName);
+
+const fileInput = document.getElementById('xml-upload');
+
+fileInput.addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+
+    const arrayBuffer = await file.arrayBuffer();
+    const byteArray = new Uint8Array(arrayBuffer);
+
+    console.log("Passing XML data across the Wasm boundary to the C# Engine...");
+
+    const jsonResult = exports.OEventHelperWeb.EngineBridge.Prioritize(byteArray, 3, false, []);
+    const result = JSON.parse(jsonResult);
+
+    console.log("Engine finished:", result);
+});
